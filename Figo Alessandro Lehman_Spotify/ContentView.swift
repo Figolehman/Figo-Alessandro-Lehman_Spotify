f5ff59f7259f3b141e5279567a554a918dcbcd84
Figo Lehman <96614364+Figolehman@users.noreply.github.com>
2024-12-11 11:46:23 +0700

    Initial Commit

---
+++ Figo Alessandro Lehman_Spotify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Figo Alessandro Lehman_Spotify
+//
+//  Created by Figo Alessandro Lehman on 11/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Figo Alessandro Lehman_Spotify/Figo_Alessandro_Lehman_SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Figo_Alessandro_Lehman_SpotifyApp.swift
+//  Figo Alessandro Lehman_Spotify
+//
+//  Created by Figo Alessandro Lehman on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Figo_Alessandro_Lehman_SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
