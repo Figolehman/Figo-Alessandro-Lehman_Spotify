76e8722be7b9ee489976b1effc16c4027a04b2d4
Figo Lehman <96614364+Figolehman@users.noreply.github.com>
2024-12-11 20:09:20 +0700

    feat(playlist-detail): Add playlist detail view

--- Figo Alessandro Lehman_Spotify/Feature/Playlist/View/PlaylistDetailView.swift
+++ Figo Alessandro Lehman_Spotify/Feature/Playlist/View/PlaylistDetailView.swift
@@ -8,12 +8,61 @@
 import SwiftUI
 
 struct PlaylistDetailView: View {
+  @EnvironmentObject var router: LibraryRouter
   let id: String
+
+  init(id: String) {
+    self.id = id
+  }
+
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    VStack(spacing: 75) {
+      VStack(alignment: .leading, spacing: 9) {
+        Text("My first library")
+          .foregroundColor(.primaryText)
+
+        Text("0 song")
+          .foregroundColor(.secondaryText)
+
+        Spacer()
+      }
+    }
+    .padding(.horizontal, 16)
+    .padding(.top, 26)
+    .frame(maxWidth: .infinity, alignment: .leading)
+    .background(
+      Image(.imgGradientBackground)
+        .resizable()
+        .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight)
+        .ignoresSafeArea()
+    )
+    .navigationBarBackButtonHidden()
+    .toolbar {
+      ToolbarItem(placement: .topBarLeading) {
+        Button {
+          router.navigateBack()
+        } label: {
+          Image(.icLeftArrow)
+            .resizable()
+            .frame(width: 20, height: 14)
+        }
+      }
+      ToolbarItem(placement: .topBarTrailing) {
+        Button {
+          router.navigate(to: .addPlaylistSong(playlistID: id))
+        } label: {
+          Image(.icPlus)
+            .resizable()
+            .frame(width: 26, height: 26)
+        }
+      }
+    }
   }
 }
 
 #Preview {
-  PlaylistDetailView(id: "")
+  NavigationView {
+    PlaylistDetailView(id: "0")
+      .environmentObject(LibraryRouter())
+  }
 }
