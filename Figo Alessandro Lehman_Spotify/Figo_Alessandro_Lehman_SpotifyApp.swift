f1d13b954ff5f8546778cac84f71ae0bebb3e7e3
Figo Lehman <96614364+Figolehman@users.noreply.github.com>
2024-12-13 11:18:25 +0700

    build(dependency): Remove netfox dependency (unused)

--- Figo Alessandro Lehman_Spotify/Figo_Alessandro_Lehman_SpotifyApp.swift
+++ Figo Alessandro Lehman_Spotify/Figo_Alessandro_Lehman_SpotifyApp.swift
@@ -5,15 +5,10 @@
 //  Created by Figo Alessandro Lehman on 11/12/24.
 //
 
-import netfox
 import SwiftUI
 
 @main
 struct Figo_Alessandro_Lehman_SpotifyApp: App {
-  init() {
-    NFX.sharedInstance().start()
-  }
-
   var body: some Scene {
     WindowGroup {
       MainTabBarView()
